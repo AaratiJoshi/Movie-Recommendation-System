dcc51451c58fff80c3552103f012e99d425263bf
AaratiJoshi <joshiaarati89@gmail.com>
2026-05-10 01:05:31 +0530

    app.py

--- app.py
+++ app.py
@@ -1,39 +1,3 @@
-
-# ------------------------------- only text --------------------------------------------------------------
-
-# import streamlit as st
-# import pickle
-
-
-# def recommend(movie):
-#     movie_index = movies_list[movies_list["title"] == movie].index[0]
-#     distances = similarity[movie_index]
-
-#     similar_movies = sorted(
-#         list(enumerate(distances)), reverse=True, key=lambda x: x[1]
-#     )[1:6]
-
-#     recommended_movies = []
-
-#     for i in similar_movies:
-#         recommended_movies.append(movies_list.iloc[i[0]].title)
-
-#     return recommended_movies
-
-
-# movies_list = pickle.load(open("movies.pkl", "rb"))
-# similarity = pickle.load(open("similarity.pkl", "rb"))
-
-# st.title("Movie Recommender System")
-
-# option = st.selectbox("Select a movie", movies_list["title"].values)
-
-# if st.button("Recommend"):
-#     recommendations = recommend(option)
-#     for movie in recommendations:
-#         st.write(movie)
-
-# ------------------------------------chatdpt------------------------------------
 import streamlit as st
 import pickle
 import requests
@@ -121,27 +85,6 @@ if st.button("Recommend"):
 
     names, posters = recommend(option)
 
-    # col1, col2, col3, col4, col5 = st.columns(5)
-
-    # with col1:
-    #     st.header(names[0])
-    #     st.image(posters[0])
-
-    # with col2:
-    #     st.header(names[1])
-    #     st.image(posters[1])
-
-    # with col3:
-    #     st.header(names[2])
-    #     st.image(posters[2])
-
-    # with col4:
-    #     st.header(names[3])
-    #     st.image(posters[3])
-
-    # with col5:
-    #     st.header(names[4])
-    #     st.image(posters[4])
     cols = st.columns(5)
 
     for idx, col in enumerate(cols):
